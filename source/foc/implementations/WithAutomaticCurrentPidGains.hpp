5bb64327413ca2a06a8fbb82b6ace917c5dd6560
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-01-19 13:43:45 +0000

    fix coverage

--- source/foc/implementations/WithAutomaticCurrentPidGains.hpp
+++ source/foc/implementations/WithAutomaticCurrentPidGains.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "foc/interfaces/Units.hpp"
+#include "source/foc/implementations/SpeedControllerImpl.hpp"
+#include "source/foc/implementations/TorqueControllerImpl.hpp"
 #include "source/foc/interfaces/FieldOrientedController.hpp"
 #include <numbers>
 
@@ -16,8 +18,8 @@ namespace foc
         void SetPidBasedOnResistanceAndInductance(Volts Vdc, Ohm resistance, MilliHenry inductance, float nyquistFactor)
         {
             auto wc = CurrentLoopBandwidth(this->BaseFrequency(), nyquistFactor);
-            auto kp = static_cast<float>(inductance.Value()) * 0.001f * wc;
-            auto ki = static_cast<float>(resistance.Value()) * wc;
+            auto kp = inductance.Value() * 0.001f * wc;
+            auto ki = resistance.Value() * wc;
 
             auto dPid = controllers::PidTunings<float>{ kp, ki, 0.0f };
             auto qPid = controllers::PidTunings<float>{ kp, ki, 0.0f };
@@ -28,9 +30,12 @@ namespace foc
     private:
         static float CurrentLoopBandwidth(hal::Hertz baseFrequency, float nyquistFactor)
         {
-            return (static_cast<float>(baseFrequency.Value()) / nyquistFactor) * 2.0f * std::numbers::pi_v<float>;
+            return (baseFrequency.Value() / nyquistFactor) * 2.0f * std::numbers::pi_v<float>;
         }
     };
+
+    extern template class WithAutomaticCurrentPidGains<TorqueControllerImpl>;
+    extern template class WithAutomaticCurrentPidGains<SpeedControllerImpl>;
 
     template<typename T>
     struct IsWithAutomaticCurrentPidGains
