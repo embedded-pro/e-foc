cf7c973a15027f0cd1d2ed10c2d85f8486619eff
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-03-07 20:53:52 +0000

    update release

---
+++ source/tool/simulator/view/gui/ParametersPanel.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "source/tool/simulator/model/Model.hpp"
+#include <QLabel>
+#include <QWidget>
+#include <optional>
+
+namespace simulator
+{
+    class ParametersPanel
+        : public QWidget
+    {
+        Q_OBJECT
+
+    public:
+        struct LoopPid
+        {
+            float kp;
+            float ki;
+            float kd;
+        };
+
+        struct PidParameters
+        {
+            LoopPid current;
+            std::optional<LoopPid> speed;
+            std::optional<LoopPid> position;
+        };
+
+        ParametersPanel(const ThreePhaseMotorModel::Parameters& motorParameters, const PidParameters& pidParameters, QWidget* parent = nullptr);
+
+        void UpdatePidParameters(const PidParameters& pidParameters);
+
+    private:
+        QLabel* resistanceLabel;
+        QLabel* inductanceLabel;
+        QLabel* frictionLabel;
+        QLabel* inertiaLabel;
+
+        QLabel* currentKpLabel;
+        QLabel* currentKiLabel;
+        QLabel* speedKpLabel = nullptr;
+        QLabel* speedKiLabel = nullptr;
+        QLabel* speedKdLabel = nullptr;
+        QLabel* positionKpLabel = nullptr;
+        QLabel* positionKiLabel = nullptr;
+        QLabel* positionKdLabel = nullptr;
+    };
+}
